d57b3c2668e1cee36a19621c26e5e4b88e5cf523
Artem_diakunchak <artem.diakunchak@student.kdg.be>
2026-06-23 06:26:30 +0200

    check

--- src/data/windows.ts
+++ src/data/windows.ts
@@ -4,6 +4,6 @@ export const WINDOWS: WindowMemory[] = [
     { slug: 'vlaaikensgang', location: 'Vlaaikensgang', videoSrc: '/videos/windows/vlaaikensgang.mp4' },
     { slug: 'het-steen-gardens', location: 'Het Steen Gardens', videoSrc: '/videos/windows/het-steen.mp4' },
     { slug: 'borgerhout-backstreets', location: 'Borgerhout Backstreets', videoSrc: '/videos/windows/borgerhout.mp4' },
-    { slug: 'sint-annatunnel-interior', location: 'Sint-Annatunnel Interior', videoSrc: '/videos/windows/sint-anna.mp4' },
+    { slug: 'sint-annatunnel-interior', location: 'Sint-Annatunnel Interior', videoSrc: '/videos/windows/sint-anna.mp4',isLive: true },
     { slug: 'zomerfabriek', location: 'Zomerfabriek', videoSrc: '/videos/windows/zomerfabriek.mp4' },
 ];
